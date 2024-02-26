d647eb15867c96dd2fa24c6e0d4f5dc3555d3fb0
Arina Vasileva <arina.vasileva.ed@gmail.com>
2024-02-26 13:52:01 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ int ghostXp = 0;
 int ghostLevel = 0;
 
 std::string ghostName[] = { "Ghastly Gale", "Soul Shroud", "Wandering Wight" }; // subject to change
+int currentGhostNames = 2;
 std::string currentghost = "";
 
 int main() {
@@ -63,12 +64,59 @@ void HUD() {
 	system("cls");
 	std::cout << "Name: " << name << "      Health:  " << totalHealth << "\nRace:  " << race
 		<< "\nSex: " << sex << "\nLevel: " << xp << "\nXP: " << xp << "\nXP to Level: " << nextLevel << std::endl;
+	Moving();
+}
+
+void CombatHUD() {
+	Sleep(500);
+	system("cls");
+	std::cout << "Name: " << name << "		|		Ghost Name:  " << currentghost << "\nHealth:  " << totalHealth
+		<< "		|		Ghost Health: " << ghostHp << "\nLevel: " << level << "		|		Ghost Level: " << ghostLevel << std::endl;
+	Moving();
+}
+
+void Combat() {
+
+	CombatHUD();
+	int playerAttack;
+	int playerDamage = 8 * level / 2;
+	int ghostAttack = 6 * ghostLevel / 2;
+
+	if (totalHealth >= 1 && ghostHp >= 1) {
+		//std::cout << "Talk\n"; need to figure out how to ask a monster to tell a riddle
+		std::cout << "1.Attack\n";
+		std::cout << "2.Block\n";
+		std::cout << "3.Run\n";
+		std::cout << "\n";
+		std::cin >> playerAttack;
+
+		//Attack
+		if (playerAttack == 1) {
+			std::cout << "Attacking...You did "
+
+		}
+		//Block
+		else if (playerAttack == 2) {
+
+		}
+		//Run
+		else if (playerAttack == 3) {
+
+		}
+		else {
+			std::cout << "Invalid Input\n";
+			Sleep(700);
+			Combat();
+		}
+
+	}
+
 }
 
 void Moving() {
 
 	int choice;
-
+	std::cout << "\n\n";
 	std::cout << "1. Move Forward \n";
 	std::cout << "2. Move Backwards \n";
 	std::cout << "3. Move Right \n";
@@ -79,18 +127,91 @@ void Moving() {
 	std::cin >> choice;
 
 	if (choice == 1) {
+		int temp = rand() % 100 + 1;
+		std::cout << "You start moving forward...\n";
+		if (temp >= 50) {
+			// Encounter a Ghost
+			CreateGhost();
+			std::string tempName = ghostName[rand() % currentGhostNames];
+			std::cout << "A" << tempName << "! Prepare to fight!\n";
+			currentghost = tempName;
+			Sleep(1000);
+			Combat();
+
+		}
+
+		std::cout << "You found nothing\n";
+		Sleep(1000);
+		HUD();
 
 	}
 	else if (choice == 2) {
+		int temp = rand() % 100 + 1;
+		std::cout << "You start moving backwards...\n";
+		if (temp >= 50) {
+			// Encounter a Ghost
+			CreateGhost();
+			std::string tempName = ghostName[rand() % currentGhostNames];
+			std::cout << "A" << tempName << "! Prepare to fight!\n";
+			currentghost = tempName;
+			Sleep(1000);
+			Combat();
+
+		}
+
+		std::cout << "You found nothing\n";
+		Sleep(1000);
+		HUD();
 
 	}
 	else if (choice == 3) {
+		int temp = rand() % 100 + 1;
+		std::cout << "You start moving to the right...\n";
+		if (temp >= 50) {
+			// Encounter a Ghost
+			CreateGhost();
+			std::string tempName = ghostName[rand() % currentGhostNames];
+			std::cout << "A" << tempName << "! Prepare to fight!\n";
+			currentghost = tempName;
+			Sleep(1000);
+			Combat();
+
+		}
+
+		std::cout << "You found nothing\n";
+		Sleep(1000);
+		HUD();
 
 	}
 	else if (choice == 4) {
 
+		int temp = rand() % 100 + 1;
+		std::cout << "You start moving to the left...\n";
+		if (temp >= 50) {
+			// Encounter a Ghost
+			CreateGhost();
+			std::string tempName = ghostName[rand() % currentGhostNames];
+			std::cout << "A" << tempName << "! Prepare to fight!\n";
+			currentghost = tempName;
+			Sleep(1000);
+			Combat();
+
+		}
+
+		std::cout << "You found nothing\n";
+		Sleep(1000);
+		HUD();
+
 	}
 	else if (choice == 5) {
+
+		std::cout << "It is time to rest in the closest corner\n";
+		if (totalHealth <= 99){
+			totalHealth += 10 * level;
+		}
+		std::cout << "You healed by resting. Your health is now" << totalHealth << std::endl;
+		sleep(1000);
+		HUD();
 
 	}
 	else {
@@ -98,4 +219,28 @@ void Moving() {
 		Sleep(700);
 		Moving();
 	}
+}
+
+void CreateGhost() {
+
+	int ghostHp = 30;
+	int ghostLevel = (rand() % 3) + level;
+
+	//if (ghostLevel == 0) {
+	//	ghostLevel = (rand() % 3) + level;
+	//}
+
+	// The value will be modified later 
+	ghostHp = (rand() % 30) * totalHealth;
+
+	//if (ghostHp == 0) {
+	//	ghostHP = = (rand() % 30) * totalHealth;
+	//}
+
+	ghostXp = ghostHp;
+
+	if (ghostHp == 0)
+		CreateGhost();
+	if (ghostLevel == 0)
+		CreateGhost();
 }
